bddfe70b0f3d4c6e7002ce25e96b40bb152ba5a3
waloro <walterof1@hotmail.com>
2020-03-30 16:17:19 -0600

    add method for decode in cesar
    add function public and private for decode algorithm cesar

--- LAB05_ED2/Class/Cesar.cs
+++ LAB05_ED2/Class/Cesar.cs
@@ -23,6 +23,21 @@ namespace LAB05_ED2.Class
             }
         }//End method for encode the algorithm cesar
 
+        public void Decode(string rPath, string wPath, string key)
+        {
+            Dictionary<byte, byte> Dictionary = GDDictionary(key);//Validate that the key doesnt contains repited values.
+
+            using (FileStream Rfile = new FileStream(rPath, FileMode.Open))
+            using (BinaryReader BR = new BinaryReader(Rfile))
+            using (FileStream Wfile = new FileStream(wPath, FileMode.Create))
+            using (BinaryWriter BW = new BinaryWriter(Wfile))
+            {
+                WCText(Dictionary, BR, BW);
+            }
+        }//End method for decode the algorithm cesar
+
+
+
         //END PUBLIC FUNCTIONS
 
 
@@ -59,13 +74,34 @@ namespace LAB05_ED2.Class
                 if (Dictionary.ContainsKey(NewByte)) bw.Write(Dictionary[NewByte]);
                 else bw.Write(NewByte);
             }
-        }//End method for write cyphed the text in encode the algorithm cesar
+        }//End method for write cyphed the text in encode and decode the algorithm cesar
 
 
         //FUNCTIONS FOR DECODE 
 
+        private Dictionary<byte, byte> GDDictionary(string key)
+        {
+            Dictionary<byte, byte> RtrnDict = new Dictionary<byte, byte>();
+            byte[] Alphabet = new byte[]
+            {
+                65, 97, 66, 98, 67, 99, 68, 100, 69, 101, 70, 102, 71, 103, 72, 104, 73, 105, 74, 106, 75, 107, 76, 108, 77, 109, 78, 110, 79, 111,
+                80, 112, 81, 113, 82, 114, 83, 115, 84, 116, 85, 117, 86, 118, 87, 119, 88, 120, 89, 121, 90, 122
+            };
 
+            for (int i = 0; i < key.Length; i++) RtrnDict.Add((byte)key[i], Alphabet[i]);
 
+            int counter = key.Length;
+            foreach (var item in Alphabet)
+            {
+                if (!RtrnDict.ContainsKey(item))
+                {
+                    RtrnDict.Add(item, Alphabet[counter]);
+                    counter++;
+                }
+            }
+
+            return RtrnDict;
+        }//End method for generate decryption in the dictionary in the decode
 
         //END PRIVATE FUNCTIONS
 
